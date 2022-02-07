a846d59eabd9759e8b712f52a629c5758a6cb0aa
José Miguel Sánchez Fernández <jose.miguel@visualstudioex3.com>
2022-02-07 17:15:25 +0100

    Implemented US3 UATs
    - Implemented a IDice mock service for UAT2.

---
+++ Tests/SnakesAndLadders.Tests/Mocks/Mock4Dice.cs
@@ -0,0 +1,11 @@
+﻿using SnakesAndLadders.BackEnd.Contracts.Services;
+
+namespace SnakesAndLadders.Tests.Mocks
+{
+    public class Mock4Dice : IDice
+    {
+        #region Methods & Functions
+        public int Roll() => 4;
+        #endregion
+    }
+}
