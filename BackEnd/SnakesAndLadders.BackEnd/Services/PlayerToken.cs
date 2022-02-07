71f6272416d09bbf9ea28f42081251c9d0c110f6
José Miguel Sánchez Fernández <jose.miguel@visualstudioex3.com>
2022-02-07 14:19:00 +0100

    Created IPlayerToken service implementation
    - Added new constants to BoardConstants class.

---
+++ BackEnd/SnakesAndLadders.BackEnd/Services/PlayerToken.cs
@@ -0,0 +1,79 @@
+﻿using SnakesAndLadders.BackEnd.Contracts.Constants;
+using SnakesAndLadders.BackEnd.Contracts.Enums;
+using SnakesAndLadders.BackEnd.Contracts.Models;
+using SnakesAndLadders.BackEnd.Contracts.Services;
+using System;
+
+namespace SnakesAndLadders.BackEnd.Services
+{
+    internal class PlayerToken : IPlayerToken
+    {
+        #region Internal vars
+        private readonly IBoard _board;
+        #endregion
+
+        #region Properties
+        public int Position { get; private set; }
+        #endregion
+
+        #region Events
+        public event Action OnPlayerWins;
+        #endregion
+
+        public PlayerToken(IBoard board)
+        {
+            this._board = board;
+            this.Reset();
+        }
+
+        #region Methods & Functions
+        public void Move(int tiles)
+        {
+            if (tiles < 1)
+                throw PlayerToken.FormatNegativeTilesToMoveException(argumentName: nameof(tiles), argumentValue: tiles);
+
+            this.Position += tiles;
+
+            this.FixPositionIfPlayerIsAheadOfFinishTile();
+            this.JumpToTargetTileIfPlayerIsOnSnakeOrLadderTile();
+            this.NotifyWinEventIfPlayerIsOnFinishTile();
+        }
+
+        public void Reset() => this.Position = BoardConstants.START_TILE_NUMBER;
+
+        private bool IsCurrentPositionSnakeOrLadderTile(out int targetTileNumber)
+        {
+            BoardTile tile = this._board.GetTileByNumberPosition(this.Position);
+            targetTileNumber = 0;
+
+            if (tile.type != BoardTileTypes.Normal)
+                targetTileNumber = tile.jumpToTileNumber;
+
+            return targetTileNumber > 0;
+        }
+
+        private void FixPositionIfPlayerIsAheadOfFinishTile()
+        {
+            if (this.Position > BoardConstants.FINISH_TILE_NUMBER)
+                this.Position = BoardConstants.FINISH_TILE_NUMBER - (this.Position - BoardConstants.FINISH_TILE_NUMBER);
+        }
+
+        private void JumpToTargetTileIfPlayerIsOnSnakeOrLadderTile()
+        {
+            if (this.IsCurrentPositionSnakeOrLadderTile(out int targetTileNumber))
+                this.Position = targetTileNumber;
+        }
+
+        private void NotifyWinEventIfPlayerIsOnFinishTile()
+        {
+            if (this.Position == BoardConstants.FINISH_TILE_NUMBER)
+                this.OnPlayerWins?.Invoke();
+        }
+
+        private static ArgumentException FormatNegativeTilesToMoveException(string argumentName, int argumentValue)
+        {
+            return new($"{nameof(IPlayerToken)}::{nameof(IPlayerToken.Move)}: The \"{argumentName}\" argument value must be a value over 0 ({argumentName}: {argumentValue}).");
+        }
+        #endregion
+    }
+}
